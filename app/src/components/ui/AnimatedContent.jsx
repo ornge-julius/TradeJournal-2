e23d95ef4c8c24a4e704acea23f44c0dab9205da
Julius Ware <jcware@covermymeds.com>
2025-11-17 11:37:03 -0500

    Remove duplicate package files

--- app/src/components/ui/AnimatedContent.jsx
+++ app/src/components/ui/AnimatedContent.jsx
@@ -1,6 +1,6 @@
 import { useRef, useEffect } from 'react';
 import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
 
